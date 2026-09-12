3846fae735c715e72e2f5cd45448b9951a106bb0
minaspace280-cmd <minaspace280@gmail.com>
2026-09-12 23:54:05 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-#include <iostream>
 #include "crow.h"
+#include "nlohmann/json.hpp"
+#include "controller/EmployeeApiController.h"
+#include "controller/InventoryApiController.h"
 #include "controller/PaymentApiController.h"
 #include "controller/ReportApiController.h"
 int main() {
     crow::SimpleApp app;
+    EmployeeApiController empApi;
+    InventoryApiController invApi;
     PaymentApiController paymentApi;
     ReportApiController reportApi;
+    CROW_ROUTE(app, "/api/employees").methods(crow::HTTPMethod::GET)([&empApi]() {
+        return crow::response(empApi.getAllEmployees().dump());
+    });
+    CROW_ROUTE(app, "/api/employees").methods(crow::HTTPMethod::POST)([&empApi](const crow::request& req) {
+        auto jsonReq = nlohmann::json::parse(req.body);
+        return crow::response(empApi.createEmployee(jsonReq).dump());
+    });
+    CROW_ROUTE(app, "/api/inventory").methods(crow::HTTPMethod::GET)([&invApi]() {
+        return crow::response(invApi.getAllItems().dump());
+    });
+
+    CROW_ROUTE(app, "/api/inventory").methods(crow::HTTPMethod::POST)([&invApi](const crow::request& req) {
+        auto jsonReq = nlohmann::json::parse(req.body);
+        return crow::response(invApi.createItem(jsonReq).dump());
+    });
+    CROW_ROUTE(app, "/api/inventory/low-stock").methods(crow::HTTPMethod::GET)([&invApi]() {
+        return crow::response(invApi.getLowStockItems().dump());
+    });
     CROW_ROUTE(app, "/api/payment").methods(crow::HTTPMethod::POST)([&paymentApi](const crow::request& req) {
         return paymentApi.handlePayment(req);
     });
     CROW_ROUTE(app, "/api/reports/revenue").methods(crow::HTTPMethod::GET)([&reportApi](const crow::request& req) {
         return reportApi.getRevenueReport(req);
     });
-    std::cout << "App is running on port 18080..." << std::endl;
-    app.port(18080).multithreaded().run();
-    return 0;
+    app.port(8080).multithreaded().run();
 }
