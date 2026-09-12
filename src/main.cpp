f80ecb747a65b00d0191f6436963fd82beb3aef0
minaspace280-cmd <minaspace280@gmail.com>
2026-09-12 23:37:01 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,16 @@
 #include "crow.h"
 #include "controller/PaymentApiController.h"
 #include "controller/ReportApiController.h"
-
 int main() {
     crow::SimpleApp app;
-
-    // Khởi tạo Controller của bạn
     PaymentApiController paymentApi;
     ReportApiController reportApi;
-
-    // API UC11 - Checkout & Payment Processing
     CROW_ROUTE(app, "/api/payment").methods(crow::HTTPMethod::POST)([&paymentApi](const crow::request& req) {
         return paymentApi.handlePayment(req);
     });
-
-    // API Báo cáo doanh thu
     CROW_ROUTE(app, "/api/reports/revenue").methods(crow::HTTPMethod::GET)([&reportApi](const crow::request& req) {
         return reportApi.getRevenueReport(req);
     });
-
     std::cout << "App is running on port 18080..." << std::endl;
     app.port(18080).multithreaded().run();
     return 0;
